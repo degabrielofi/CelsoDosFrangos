5e39c2b834d92c63b42732475c47a9bcf8c106d7
Gabriel Silva <yzgabrieel02@gmail.com>
2024-04-04 18:07:57 -0300

    componets `Cards` finished

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Card } from "./style";
 
 const Cards = () => {
-  return <div>Cards</div>;
+  return (
+    <>
+      <Card>
+        <section className="container">
+          <div className="content">
+            <span className="number">849</span>
+            <h3 className="title">Moções</h3>
+          </div>
+          <div className="content">
+            <span className="number">151</span>
+            <h3 className="title">Moções</h3>
+          </div>
+          <div className="content">
+            <span className="number">995</span>
+            <h3 className="title">Moções</h3>
+          </div>
+          <div className="content">
+            <span className="number">465</span>
+            <h3 className="title">Moções</h3>
+          </div>
+        </section>
+      </Card>
+    </>
+  );
 };
 
 export default Cards;
--- src/components/Cards/style.jsx
+++ src/components/Cards/style.jsx
@@ -1,1 +1,37 @@
 import styled from "styled-components";
+
+export const Card = styled.div`
+  margin-top: 5%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+
+  .container {
+    width: 60%;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: 1fr;
+    grid-column-gap: 25px;
+  }
+
+  .content {
+    padding: 2.5rem;
+    text-align: center;
+    background: #001238;
+    border-radius: 1rem;
+  }
+
+  span {
+    font-weight: 700;
+    color: #fff;
+    font-size: 300%;
+  }
+
+  h3 {
+    text-transform: uppercase;
+    color: #fff;
+    font-size: 100%;
+    flex-direction: column;
+    flex-wrap: wrap;
+  }
+`;
